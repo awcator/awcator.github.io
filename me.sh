ff46dc3ae3a7dc974d35a06269c4ee82a6ad47c6
awcator <54628909+awcator@users.noreply.github.com>
2019-08-28 18:14:46 +0530

    Initial

---
+++ me.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+echo "Hey There!, Whats up?"
+echo "btw am Devayya S.M, this is my profile dipicting in linux terminal theme.";
+echo "and I love doing Pentesting, Programming and Gamming. Presently studying in GECH, HASSAN";
+echo "you can also find me here on, ";
+echo "Cheers ";
+echo "Made with love by Devayya";
